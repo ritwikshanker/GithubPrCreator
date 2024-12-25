df31512a2c4aae29a833b838b9a571c01616d8f3
ritwikshanker <ritwikshanker@yahoo.in>
2024-12-25 14:19:00 +0530

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.kohsuke:github-api:1.315")
+    implementation("org.kohsuke:github-api:1.326")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
     implementation("org.slf4j:slf4j-api:2.0.9")
-    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("ch.qos.logback:logback-classic:1.5.15")
 
 }
 
